1cf161a8416521964267ddddc82a4575c9a773fd
Daiane Correa <daiane.ocorrea@gmail.com>
2018-05-09 16:38:40 -0300

    Creating the Book Model

--- src/com/twu/biblioteca/book/Book.java
+++ src/com/twu/biblioteca/book/Book.java
@@ -1,4 +1,20 @@
 package com.twu.biblioteca.book;
 
 public class Book {
+    private final String name;
+
+    public Book(String book1) {
+        this.name = book1;
+    }
+
+    @Override
+    public boolean equals(Object book) {
+        Book otherBook = (Book) book;
+        return name.equals(otherBook.name);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
