35b18622057e837cc03d44828bc287e9da0ed341
Daiane Correa <daiane.ocorrea@gmail.com>
2018-05-09 16:38:17 -0300

    Creating the Book Controller for the Test

--- src/com/twu/biblioteca/book/BookController.java
+++ src/com/twu/biblioteca/book/BookController.java
@@ -1,4 +1,16 @@
 package com.twu.biblioteca.book;
 
+import java.util.List;
+
 public class BookController {
+
+    private BookDataRepository bookDataRepository;
+
+    public BookController(BookDataRepository bookDataRepository) {
+        this.bookDataRepository = bookDataRepository;
+    }
+
+    public List<Book> getAllBooks() {
+        return bookDataRepository.getAllBooks();
+    }
 }
