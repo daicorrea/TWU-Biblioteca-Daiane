b7079e2d1dd8f75290a29463a4e188025717bd01
Daiane Correa <daiane.ocorrea@gmail.com>
2018-05-10 14:54:56 -0300

    Creating Repository

--- src/com/twu/biblioteca/database/RepositoryAPI.java
+++ src/com/twu/biblioteca/database/RepositoryAPI.java
@@ -1,5 +1,15 @@
-package com.twu.biblioteca.book;
+package com.twu.biblioteca.database;
+
+import com.twu.biblioteca.book.Book;
+
+import java.util.ArrayList;
 
 public class RepositoryAPI {
 
+    public ArrayList getAllBooks() {
+        ArrayList<Book> bookList = new ArrayList<>();
+        bookList.add(new Book("Book1"));
+        bookList.add(new Book("Book2"));
+        return bookList;
+    }
 }
