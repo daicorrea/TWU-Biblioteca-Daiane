4387ddc9314e60a56bed59c65e6291dd8b4e7998
Daiane Correa <daiane.ocorrea@gmail.com>
2018-05-10 10:14:09 -0300

    Splitting the Main Class

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,12 +1,24 @@
 package com.twu.biblioteca;
 
 import com.twu.biblioteca.Menu.Message;
+import com.twu.biblioteca.Menu.Printable;
 
 public class BibliotecaApp {
 
-    public static void main(String[] args) {
+    private Printable message;
 
-        Message message = new Message();
-        System.out.println(message.showWelcomeMessage());
+    public BibliotecaApp() {
+        this(new Message());
+    }
+
+    BibliotecaApp(Printable message){
+        this.message = message;
+    }
+
+    public void printWelcome() {
+        String welcome = message.showWelcomeMessage();
+        message.showWelcomeMessage();
+        System.out.println(welcome);
+
     }
 }
--- src/com/twu/biblioteca/Main.java
+++ src/com/twu/biblioteca/Main.java
@@ -1,4 +1,8 @@
 package com.twu.biblioteca;
 
 public class Main {
+    public static void main(String [] args ){
+        BibliotecaApp app = new BibliotecaApp();
+        app.printWelcome();
+    }
 }
